7f172bdead3b919201b4f6f38b691be362904b9a
Thiago Prado Loureiro <thiago9889@gmail.com>
2018-06-13 23:47:43 -0300

    Handler Login

---
+++ src/Handler/Login.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+module Handler.Login where
+
+import Import 
+import Text.Lucius
+import Database.Persist.Postgresql
+
+formLogin :: Form (Text, Text)
+formLogin = renderDivs $ (,) 
+        <$> areq emailField "Email: " Nothing
+        <*> areq passwordField "Senha: " Nothing
+        
+getLoginR :: Handler Html
+getLoginR = do 
+    (widget,enctype) <- generateFormPost formLogin
+    msg <- getMessage
+    defaultLayout $ do 
+        toWidget $(luciusFile "templates/home.lucius")
+        [whamlet|
+            <h1>
+            <h2>
+                Acessar Conta
+            $maybe mensa <- msg
+                ^{mensa}
+            <br>
+            <form action=@{LoginR} method=post enctype=#{enctype}>
+                ^{widget}
+                <input type="submit" value="OK">
+        |]
