13357d69248abce0755f36901fbeebd4a173aab9
Thiago Prado Loureiro <thiago9889@gmail.com>
2018-06-14 00:06:40 -0300

    Handler Nivel

---
+++ src/Handler/Nivelb.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+module Handler.Nivelb where
+
+import Import 
+import Text.Lucius
+import Database.Persist.Postgresql
+
+formNivelb :: Form Nivelb 
+formNivelb = renderDivs $ Nivelb 
+        <$> areq textField "Nome: " Nothing
+
+getCadNivelbR :: Handler Html
+getCadNivelbR = do 
+    (widget,enctype) <- generateFormPost formNivelb
+    defaultLayout $ do 
+        toWidget $(luciusFile "templates/home.lucius")
+        $(whamletFile "templates/menu.hamlet")
+        [whamlet|
+            <h2>
+                Inserir Nível
+            <form action=@{CadNivelbR} method=post enctype=#{enctype}>
+                ^{widget}
+                <br>
+                <br>
+                <input type="submit" value="OK">
+        |]
